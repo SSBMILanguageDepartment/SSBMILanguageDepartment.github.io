2bad41d291dcd78e1c6c2445a20c671c949b709e
SSBMILanguageDepartment <109108019+SSBMILanguageDepartment@users.noreply.github.com>
2026-01-06 15:25:30 -0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,7 +106,6 @@ function popupFunction(event) {
 
 
 
-
 // FUNCTION FOR COPYING TEXT TO CLIPBOARD
 
 function copy_to_clipboard( elm_id ) {
@@ -142,6 +141,34 @@ function copyToClipboard_m() {
   navigator.clipboard.writeText(copyText.value)
 }
 
+// FUNCTION FOR COPYING TEXT FROM INPUT FIELD TO CLIPBOARD [[FOR NORTHERN SIERRA MIWOK]]
+
+function copyToClipboard_Miw() {
+  // Get the input field element
+  var copyText = document.getElementById("myInput_Miw");
+
+  // Select the text in the input field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  navigator.clipboard.writeText(copyText.value)
+}
+
+// FUNCTION FOR COPYING TEXT FROM INPUT FIELD TO CLIPBOARD [[FOR NORTHERN SIERRA MIWOK [[FOR MOBILE]]]]
+
+function copyToClipboard_Miw_m() {
+  // Get the input field element
+  var copyText = document.getElementById("myInput_Miw_m");
+
+  // Select the text in the input field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  navigator.clipboard.writeText(copyText.value)
+}
+
+
+
 
 
 // FUNCTION FOR CHARACTER PICKER
@@ -157,6 +184,21 @@ function addLetter_m(letter) {
 	const inputField = document.getElementById("myInput_m");
 	inputField.value += letter;
 }
+
+// FUNCTION FOR CHARACTER PICKER [[FOR NORTHERN SIERRA MIWOK]]
+
+function addLetter_Miw(letter) {
+	const inputField = document.getElementById("myInput_Miw");
+	inputField.value += letter;
+}
+
+// FUNCTION FOR CHARACTER PICKER [[FOR NORTHERN SIERRA MIWOK [[FOR MOBILE]]]]
+
+function addLetter_Miw_m(letter) {
+	const inputField = document.getElementById("myInput_Miw_m");
+	inputField.value += letter;
+}
+
 
 
 
@@ -200,6 +242,49 @@ function searchTable_m() {
 		}      
 	}
 }
+
+// FUNCTION FOR SEARCHING TABLE [[FOR NORTHERN SIERRA MIWOK]]
+
+function searchTable_Miw() {
+	// Declare variables
+	var input, filter, table, tr, td1, td2, i, txtValue;
+	input = document.getElementById("myInput_Miw");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("myTable_Miw");
+	tr = table.getElementsByTagName("tr");
+
+	// Loop through all table rows, and hide those who don't match the search query
+	for (i = 1; i < tr.length; i++) {
+		if (tr[i].textContent.toUpperCase().indexOf(filter) > -1) {
+			tr[i].style.display = "";
+		} else {
+			tr[i].style.display = "none";
+		}      
+	}
+}
+
+// FUNCTION FOR SEARCHING TABLE [[FOR NORTHERN SIERRA MIWOK [[FOR MOBILE]]]]
+
+function searchTable_Miw_m() {
+	// Declare variables
+	var input, filter, table, tr, td1, td2, i, txtValue;
+	input = document.getElementById("myInput_Miw_m");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("myTable_Miw_m");
+	tr = table.getElementsByTagName("tr");
+
+	// Loop through all table rows, and hide those who don't match the search query
+	for (i = 1; i < tr.length; i++) {
+		if (tr[i].textContent.toUpperCase().indexOf(filter) > -1) {
+			tr[i].style.display = "";
+		} else {
+			tr[i].style.display = "none";
+		}      
+	}
+}
+
+
+
 
 
 
@@ -318,6 +403,120 @@ function sortTable_m(n) {
 	}
 }
 
+// FUNCTION FOR SORTING TABLE [[FOR NORTHERN SIERRA MIWOK]]
+
+function sortTable_Miw(n) {
+	var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+	table = document.getElementById("myTable_m");
+	switching = true;
+	// Set the sorting direction to ascending:
+	dir = "asc";
+	/* Make a loop that will continue until
+	no switching has been done: */
+	while (switching) {
+		// Start by saying: no switching is done:
+		switching = false;
+		rows = table.rows;
+		/* Loop through all table rows (except the
+		first, which contains table headers): */
+		for (i = 1; i < (rows.length - 1); i++) {
+			// Start by saying there should be no switching:
+			shouldSwitch = false;
+			/* Get the two elements you want to compare,
+			one from current row and one from the next: */
+			x = rows[i].getElementsByTagName("TD")[n];
+			y = rows[i + 1].getElementsByTagName("TD")[n];
+			/* Check if the two rows should switch place,
+			based on the direction, asc or desc: */
+				if (dir == "asc") {
+					if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+					// If so, mark as a switch and break the loop:
+					shouldSwitch = true;
+					break;
+				}
+			} else if (dir == "desc") {
+				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+					// If so, mark as a switch and break the loop:
+					shouldSwitch = true;
+					break;
+				}
+			}
+		}
+		if (shouldSwitch) {
+			/* If a switch has been marked, make the switch
+			and mark that a switch has been done: */
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			switching = true;
+			// Each time a switch is done, increase this count by 1:
+			switchcount ++;
+		} else {
+			/* If no switching has been done AND the direction is "asc",
+			set the direction to "desc" and run the while loop again. */
+			if (switchcount == 0 && dir == "asc") {
+				dir = "desc";
+				switching = true;
+			}
+		}
+	}
+}
+
+// FUNCTION FOR SORTING TABLE [[FOR NORTHERN SIERRA MIWOK [[FOR MOBILE]]]]
+
+function sortTable_Miw_m(n) {
+	var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+	table = document.getElementById("myTable_m");
+	switching = true;
+	// Set the sorting direction to ascending:
+	dir = "asc";
+	/* Make a loop that will continue until
+	no switching has been done: */
+	while (switching) {
+		// Start by saying: no switching is done:
+		switching = false;
+		rows = table.rows;
+		/* Loop through all table rows (except the
+		first, which contains table headers): */
+		for (i = 1; i < (rows.length - 1); i++) {
+			// Start by saying there should be no switching:
+			shouldSwitch = false;
+			/* Get the two elements you want to compare,
+			one from current row and one from the next: */
+			x = rows[i].getElementsByTagName("TD")[n];
+			y = rows[i + 1].getElementsByTagName("TD")[n];
+			/* Check if the two rows should switch place,
+			based on the direction, asc or desc: */
+				if (dir == "asc") {
+					if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+					// If so, mark as a switch and break the loop:
+					shouldSwitch = true;
+					break;
+				}
+			} else if (dir == "desc") {
+				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+					// If so, mark as a switch and break the loop:
+					shouldSwitch = true;
+					break;
+				}
+			}
+		}
+		if (shouldSwitch) {
+			/* If a switch has been marked, make the switch
+			and mark that a switch has been done: */
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			switching = true;
+			// Each time a switch is done, increase this count by 1:
+			switchcount ++;
+		} else {
+			/* If no switching has been done AND the direction is "asc",
+			set the direction to "desc" and run the while loop again. */
+			if (switchcount == 0 && dir == "asc") {
+				dir = "desc";
+				switching = true;
+			}
+		}
+	}
+}
+
 
 
 
@@ -339,4 +538,56 @@ function playPauseAudio(audioId) {
         audio.pause();
         currentPlayingAudio = null;
     }
+}
+
+
+
+
+
+
+
+// FUNCTION FOR LANGUAGE TABS 
+
+function openLanguage(evt, languageName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(languageName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// FUNCTION FOR LANGUAGE TABS [[FOR MOBILE]]
+
+function openLanguage_m(evt, languageName_m) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(languageName_m).style.display = "block";
+  evt.currentTarget.className += " active";
 }
